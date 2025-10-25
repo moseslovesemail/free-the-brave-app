8b463c7ab911b94b44e23499ecc8d6a0cf178850
moseslovesemail <moseslovesemail@gmail.com>
2025-10-26 10:35:32 +1300

    feat: add layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Free The Brave',
+  description: 'Wellness education aligned to Movement \u2022 Touch \u2022 Music \u2022 Meaning',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
